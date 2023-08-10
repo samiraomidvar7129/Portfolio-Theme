492f99dede9bff344fc520ab02f7ec0639aee272
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2023-08-10 02:03:33 -0700

    Create Contact Cards

---
+++ Assets/JS/MyScript.js
@@ -0,0 +1,11 @@
+// const colorBtns=document.querySelectorAll('.btn')
+
+// colorBtns.forEach(function(color){
+//     color.addEventListener('click',function(e){
+//     //  console.log(e.target.dataset.color); 
+//     let btnColor=e.target.dataset.color;
+//     document.documentElement.style.setProperty(' --theme-color',btnColor)
+    
+//     })
+// })
+
